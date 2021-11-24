d36d2f6fce864daf8931affe15c72c3121243393
bereal1995 <gygud98@gmail.com>
2021-11-24 23:49:19 +0900

    feat(Header): 헤더 추가

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+import Link from '@mui/material/Link';
+import LoginIcon from '@mui/icons-material/Login';
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Box>
+      <AppBar>
+        <Toolbar>
+          {/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link href="./" color="inherit">
+              Butler
+            </Link>
+          </Typography>
+          <Link href="./sign-in" color="#fff">
+            <LoginIcon />
+          </Link>
+        </Toolbar>
+        {/* <Link Link="./sign-in">로그인</Link>
+    <Link Link="./sign-up">회원가입</Link> */}
+      </AppBar>
+    </Box>
+  );
 }
