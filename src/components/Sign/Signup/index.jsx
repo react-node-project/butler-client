886c11741b4c30941301f7d9cbe1858ad83b483f
bereal1995 <gygud98@gmail.com>
2021-12-02 09:13:36 +0900

    feat(Sign): 스타일 수정

--- src/components/Sign/Signup/index.jsx
+++ src/components/Sign/Signup/index.jsx
@@ -1,81 +1,81 @@
 import React from 'react';
-import { Avatar, Button, TextField, Link, Grid, Box, Typography } from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import TextField from '@mui/material/TextField';
+import { Box, Button, Link, Paper, Grid, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
-import { PATH_USER_SIGNIN } from '../../../constants/PathConstants';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { StyledContainer } from '../../../styles/element.styled';
-import { StyledBox } from './Signup.styled';
+import OnTheWay from '../../../asset/img/ontheway';
 
-export default function SignUp() {
+const SignUp = (props) => {
+  const validationSchema = yup.object().shape({
+    name: yup.string().required('필수입력사항입니다'),
+    email: yup.string().email('올바른 이메일 형식이 아닙니다').required('필수입력사항입니다'),
+    password: yup
+      .string()
+      .required('필수입력사항입니다')
+      .min(5, '비밀번호는 5자리 이상이어야 합니다')
+      .max(20, '비밀번호는 20자리 이하이어야합니다')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d\s:]).*$/, {
+        message:
+          '비밀번호는 최소한 대문자 한개 (A-Z), 소문자 한개 (a-z), 숫자 (0-9) 그리고 특수문자를 포함하여야 합니다',
+      }),
+  });
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
   const onSubmit = (data) => {
     console.log('data', data);
   };
-
+  const handleFlip = () => {
+    props.handleFlip();
+  };
   return (
-    <StyledContainer maxWidth="sm">
-      <StyledBox>
-        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          회원가입
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+    <StyledContainer maxWidth="xs">
+      <Paper sx={{ px: 5, py: 8 }}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item>
+            <Typography variant="h5">Sign Up</Typography>
+            <OnTheWay />
+          </Grid>
+
+          {/* 아래는 formcontrol */}
+          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Grid item xs={12}>
               <TextField
-                autoComplete="given-name"
-                name="firstName"
+                margin="normal"
                 required
                 fullWidth
-                id="firstName"
-                label="성"
-                autoFocus
-                {...register('firstName', {
-                  required: { value: true, message: '필수입력사항입니다.' },
-                })}
-                helperText={errors.firstName?.message}
-                error={Boolean(errors.firstName)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="lastName"
+                id="name"
                 label="이름"
-                name="lastName"
+                name="name"
                 autoComplete="family-name"
-                {...register('lastName', {
-                  required: { value: true, message: '필수입력사항입니다.' },
-                })}
-                helperText={errors.lastName?.message}
-                error={Boolean(errors.lastName)}
+                {...register('name')}
+                helperText={errors.name?.message}
+                error={Boolean(errors.name)}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
+                margin="normal"
                 required
                 fullWidth
                 id="email"
                 label="이메일"
                 name="email"
                 autoComplete="email"
-                {...register('email', {
-                  required: { value: true, message: '필수입력사항입니다.' },
-                  pattern: { value: /^\S+@\S+$/i, message: '올바른 이메일 형식이 아닙니다.' },
-                })}
+                {...register('email')}
                 helperText={errors.email?.message}
                 error={Boolean(errors.email)}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
+                margin="normal"
                 required
                 fullWidth
                 name="password"
@@ -83,24 +83,30 @@ export default function SignUp() {
                 type="password"
                 id="password"
                 autoComplete="new-password"
-                {...register('password', { required: { value: true, message: '필수입력사항입니다.' } })}
+                {...register('password')}
                 helperText={errors.password?.message}
                 error={Boolean(errors.password)}
               />
             </Grid>
-          </Grid>
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-            회원가입
-          </Button>
-          <Grid container justifyContent="flex-end">
+
+            {/* Login buttons */}
             <Grid item>
-              <Link href={PATH_USER_SIGNIN} variant="body2">
-                이미 계정이 있으십니까? 로그인
-              </Link>
+              <Button sx={{ px: 16 }} type="submit" variant="contained" size="large">
+                Sign Up
+              </Button>
             </Grid>
+          </Box>
+          <Grid item>
+            <Box pt={2}>
+              <Link component="button" onClick={handleFlip} variant="body2">
+                Already have an account? SignIn
+              </Link>
+            </Box>
           </Grid>
-        </Box>
-      </StyledBox>
+        </Grid>
+      </Paper>
     </StyledContainer>
   );
-}
+};
+
+export default SignUp;
