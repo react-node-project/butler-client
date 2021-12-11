baf34873901a001920ddd2711f825d4f25961418
esther615kim <vilchen@naver.com>
2021-12-12 08:11:47 +0900

    Design: ThemeMenue.jsx

---
+++ src/components/Landing/landing.styled.js
@@ -0,0 +1,20 @@
+// import { styled } from '@mui/styles';
+import { StyledGrid } from '@mui/material';
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+position:relative;
+.title{
+position:absolute;
+display:flex;
+color:#fff;
+width:100%;
+height:70%;
+align-items:center;
+justify-content:center;
+}
+.MuiImageListItemBar{
+    border:1px solid blue;
+    text-align:left;
+}
+`
