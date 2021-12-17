fcaf1e8fcd9b169937a7a20694cfa177ac60c7f8
hoin <72450781+lucid-jin@users.noreply.github.com>
2021-12-17 21:07:11 +0900

    Change Landing page mixin LocationSearch Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setIsLoading } from '../store/features/appSlice';
 import ThemeMenu from './../components/Landing/ThemeMenu';
 import { Paper } from '@mui/material';
-import Footer from '../components/footer';
+import LocationSearch from '@components/Location/LocationSearch';
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -17,10 +17,8 @@ export default function Home() {
   return (
     <div>
       <StyledContainer maxWidth="xs">
-        <Paper sx={{ px: 5, py: 8 }}>
-          {/* <div>isLoading: {`${isLoading}`}</div>
-      <Button onClick={() => onClickButton()} variant="outlined">
-      </Button> */}
+        <Paper sx={{ px: 5, py: 5 }}>
+          <LocationSearch />
           <ThemeMenu />
           {/* <Footer /> */}
         </Paper>
