8c13105e0ed76db93fa70f19eeb59a365f0be754
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-01-10 22:37:07 +0900

    Setting Jest Config - 1. Use typescript(@types/jest, @babel/preset-typescript - 2. Use css import(identity-obj-proxy, jest.config moduleNameMapper)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./setupTests.js'],
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation)/'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
