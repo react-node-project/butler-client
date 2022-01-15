9a085cfdffc41fb1cb6796c86b846b1e55edb96d
esther615kim <vilchen@naver.com>
2022-01-16 03:19:17 +0900

    feat(favlist): add FavList component

--- src/constants/PathConstants.ts
+++ src/constants/PathConstants.ts
@@ -3,3 +3,4 @@ export const PATH_USER_SIGNIN = '/signin';
 export const PATH_RESTAURANTS = '/restaurants';
 export const PATH_RESTAURANTS_DETAIL = '/restaurant_detail';
 export const PATH_API_DOCS = '/api-docs';
+export const PATH_FAVLIST = '/favourites';
