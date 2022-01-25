80c6370632a6465abf0b3654c250225187b3aa6c
hoin <72450781+lucid-jin@users.noreply.github.com>
2022-01-25 21:35:28 +0900

    Change: build failed fixed(from path) & api-server-url changed

--- src/constants/EnvContant.ts
+++ src/constants/EnvContant.ts
@@ -1,5 +1,5 @@
 export const MOCK_API_URL = 'http://localhost:4000/api/v1' as string;
-export const API_URL = 'https://react-node-project-butler-api.herokuapp.com/api/v1/web';
+export const API_URL = 'https://react-node-project-butler-api.herokuapp.com/v1/web';
 
 export const MAP_API_KEY = process.env['REACT_APP_MAP_API_KEY'] || ('' as string);
 export const KAKAO_API_KEY = process.env['REACT_APP_KAKAO_API_KEY'] || ('' as string);
